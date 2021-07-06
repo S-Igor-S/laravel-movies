4c0c2990554f9c08422276464f8127fff5504b4f
Ihor <ihor.prachuk@gmail.com>
2021-07-06 11:37:13 +0300

    feat: add new attribute

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
 
+    /**
+     * Get the poster's path.
+     *
+     * @param array $movie
+     * @return string
+     */
+
+    public function getPosterPathAttribute()
+    {
+        return "https://image.tmdb.org/t/p/w500" . $this->Poster;
+    }
 }
