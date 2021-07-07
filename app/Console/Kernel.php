5fdfe02df7257455586a779abaa38dbb269d8e71
Ihor <ihor.prachuk@gmail.com>
2021-07-07 19:04:39 +0300

    feat: add cron task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(new AddMovies)->everyMinute();
+        $schedule->call(new AddMovies)->daily();
     }
 
     /**
