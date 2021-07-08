4e5c48e23d2b253aebc73d4887688c6edea0b0d5
Ihor <ihor.prachuk@gmail.com>
2021-07-08 09:18:09 +0300

    feat: change time between calls

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(new AddMovies)->daily();
+        $schedule->call(new AddMovies)->everySixHours();
     }
 
     /**
