ccca9d8b7988345dc804d1b42420d76dc43cfcbd
Ihor <ihor.prachuk@gmail.com>
2021-07-13 15:34:23 +0300

    feat: add pagination

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,22 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($movies as $movie)
-        <article class="offset-1">
-            <h1>
-                {{ $movie->name }}
-            </h1>
-            <h2>
-            </h2>
-            <div class="row">
-                <div class="col-sm">
-                    <img class="" src="{{ $movie->PosterPath }}" alt="poster">
+    <div class="container mt-5">
+        <div class="d-flex justify-content-center">
+            {!! $movies->links() !!}
+        </div>
+        @foreach ($movies as $movie)
+            <article class="offset-1">
+                <h1>
+                    {{ $movie->name }}
+                </h1>
+                <h2>
+                </h2>
+                <div class="row">
+                    <div class="col-sm">
+                        <img class="" src="{{ $movie->PosterPath }}" alt="poster">
+                    </div>
+                    <div class="col-sm">
+                        {{ $movie->description }}
+                    </div>
                 </div>
-                <div class="col-sm">
-                    {{ $movie->description }}
-                </div>
-            </div>
-        </article>
-        <hr>
-    @endforeach
+            </article>
+            <hr>
+        @endforeach
+        <div class="d-flex justify-content-center">
+            {!! $movies->links() !!}
+        </div>
+    </div>
 @endsection
