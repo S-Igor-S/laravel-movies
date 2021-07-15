0caeaba563b533ba9b13f66a50bf2c67ff1b5d4e
Ihor <ihor.prachuk@gmail.com>
2021-07-15 09:30:56 +0300

    feat: add new column to database

--- database/migrations/2021_07_06_085354_create_movies_table.php
+++ database/migrations/2021_07_06_085354_create_movies_table.php
@@ -19,6 +19,7 @@ class CreateMoviesTable extends Migration
             $table->text('description');
             $table->integer('tmdb_id');
             $table->string('poster');
+            $table->string('slug');
             $table->timestamps();
         });
     }
