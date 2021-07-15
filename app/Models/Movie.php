fe21dea715291fb290b16b3cdab7a8d88c396983
Ihor <ihor.prachuk@gmail.com>
2021-07-15 18:00:20 +0300

    feat: use method getRouteKeyName

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Support\Str;
 
 class Movie extends Model
 {
+
+    protected $guarded = [];
 
     /**
      * Get the poster's path.
@@ -18,5 +21,10 @@ class Movie extends Model
     public function getPosterPathAttribute()
     {
         return "https://image.tmdb.org/t/p/w500".$this->poster;
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
